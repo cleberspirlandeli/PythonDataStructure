94e117fd9bb17340c950c9fe83e9f8cebcdc8f49
Cleber Rezende Spirlandeli <compasso-cleber.spirlandeli@ailos.coop.br>
2024-02-28 16:28:10 -0300

    add examples about variable and constans in python

---
+++ KnowingPython/1_VariablesAndConstants.py
@@ -0,0 +1,37 @@
+# Inteiros: valores positivos ou negativos, que não possuem uma parte fracionária. Exemplos: 1, 30, 40, 12, -50
+# Float (real): valores positivos ou negativos, que podem possuir uma parte fracionária (também podem ser inteiros). Exemplos: 1.4, 6.7, 10.3, 100, -47
+# Caracteres (char ou string): qualquer elemento presente no teclado. Exemplos: “Maria”, “João”, ‘M’, ‘F’
+# Lógico (boleano): verdadeiro ou falso. Exemplos: true, false, 1, 0
+
+# Variáveis inteiras
+numero = -3
+numero_jogos = 14
+numero_convidados = 15
+print(numero)
+print(numero, numero_convidados)
+
+# Variáveis float (ponto flutuante)
+pi = 3.14
+numero_euler = 2.71
+escala_terremoto = -4.55
+print(pi)
+print(numero_euler)
+print(escala_terremoto)
+
+# Strings e chars
+letra = 'a'
+palavra1 = 'linguagem de programação'
+palavra2 = 'Python'
+print(letra, palavra1)
+print('Estou aprendendo uma', palavra1)
+print('Esta', palavra1, 'se chama', palavra2)
+
+# Input dados
+idade = int(input('Digite sua idade:'))
+print('Sua idade é ', idade)
+
+pH = float(input('Qual o pH do solo durante a última medição?'))
+print('O pH medido foi ', pH)
+
+nome = str(input('Qual o seu nome?'))
+print('Seu nome é', nome)
